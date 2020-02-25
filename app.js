98f760a1fb08b7d0300b84b3185f0a35ca224c30
José Tuzinkievicz <josetuzin@gmail.com>
2020-02-25 12:40:56 -0300

    clase 12 computed properties y watchers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ new Vue({
     data() {
         return {
             name: 'Bitcoin',
+            symbol: 'BTC',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
             changePercent: 10,
             color: 'f4f4f4',
@@ -21,6 +22,18 @@ new Vue({
             ],
             showPrices: false
        }
+    },
+
+    computed: {
+        title() {
+            return `${this.name} - ${this.symbol}`
+        }
+    },
+
+    watch: {
+        showPrices(newVal, oldVal) {
+            console.log( newVal, oldVal)
+        }
     },
 
     methods: {
